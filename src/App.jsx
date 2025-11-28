1b9996bff95fe4a38cd12f031fd53f72b51d2bc1
Lemuel Tadeo <lemueltadz@gmail.com>
2025-11-28 23:30:56 +0800

    practicing react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
