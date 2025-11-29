1f3e8a9344e0edd4b84f5a767b5b234e754b0dfa
Lemuel Tadeo <lemueltadz@gmail.com>
2025-11-29 13:51:58 +0800

    profile pic home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,18 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav className="w-full h-18 bg-slate-800 flex items-center justify-center text-white font-[roboto] text-xl gap-10   ">
-      <Link to="/">Home</Link>
-      <Link to="/About">About</Link>
-      <Link to="/Projects">Projects</Link>
-      <Link to="/Contact">Contact</Link>
+      <Link to="/" className=" hover:text-green-500">
+        Home
+      </Link>
+      <Link to="/About" className=" hover:text-green-500">
+        About
+      </Link>
+      <Link to="/Projects" className=" hover:text-green-500">
+        Projects
+      </Link>
+      <Link to="/Contact" className=" hover:text-green-500">
+        Contact
+      </Link>
     </nav>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import profile from "../assets/profile.JPG";
+
 export default function Home() {
   return (
     <div className="ml-[1%] mt-5">
+      <img src={profile} alt="Profile Pic" className="w-50 h-50 rounded-2xl" />
       <h1 className="text-6xl font-bold">Lemuel Tadeo</h1>
       <h5 className="text-lg font-normal ml-1">
         Full Stack Developer Specializing in React and Laravel
