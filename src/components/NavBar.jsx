9dabd874c255b2aed50ddb0130de468109df95c5
Lemuel Tadeo <lemueltadz@gmail.com>
2025-11-29 15:36:12 +0800

    tech stack assets

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="w-full h-18 bg-slate-800 flex items-center justify-center text-white font-[roboto] text-xl gap-10   ">
+    <nav className="w-full h-18 bg-slate-900 flex items-center justify-center text-white font-[roboto] text-xl gap-10  fixed top ">
       <Link to="/" className=" hover:text-green-500">
         Home
       </Link>
-      <Link to="/About" className=" hover:text-green-500">
+      <Link to="/about" className=" hover:text-green-500">
         About
       </Link>
-      <Link to="/Projects" className=" hover:text-green-500">
+      <Link to="/projects" className=" hover:text-green-500">
         Projects
       </Link>
-      <Link to="/Contact" className=" hover:text-green-500">
+      <Link to="/contact" className=" hover:text-green-500">
         Contact
       </Link>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,34 @@
+import { Links } from "react-router-dom";
 import profile from "../assets/profile.JPG";
+import React from "../assets/React.png";
+import Laravel from "../assets/Laravel.png";
+import Tailwind from "../assets/Tailwind.png";
+import PHP from "../assets/PHP.png";
 
 export default function Home() {
+  const techStack = [
+    { name: "React", logo: { React } },
+    { name: "Laravel", logo: { Laravel } },
+    { name: "Tailwind", logo: { Tailwind } },
+    { name: "PHP", logo: { PHP } },
+  ];
   return (
-    <div className="ml-[1%] mt-5">
-      <img src={profile} alt="Profile Pic" className="w-50 h-50 rounded-2xl" />
-      <h1 className="text-6xl font-bold">Lemuel Tadeo</h1>
-      <h5 className="text-lg font-normal ml-1">
-        Full Stack Developer Specializing in React and Laravel
-      </h5>
+    <div className="text-white h-screen flex flex-col items-center ">
+      <div className="profile flex flex-col items-center mt-30 ">
+        <img
+          src={profile}
+          alt="Profile Pic"
+          className="w-100 h-100 rounded-2xl "
+        />
+        <h1 className="text-6xl sm:text-7xl font-bold">Lemuel Tadeo</h1>
+        <h5 className="text-sm sm:text-lg font-normal">
+          Full Stack Developer Specializing in React and Laravel
+        </h5>
+      </div>
+
+      <div className="tech-stack-title mt-10">
+        <h2 className="text-2xl">Core Technologies</h2>
+      </div>
     </div>
   );
 }
